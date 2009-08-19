7064988cdb528010d33a6b8c92c56c362651972e
aurels <aurels@gangsta.local>
2009-08-19 15:53:43 +0200

    multistage cap

--- files/config/deploy.rb
+++ files/config/deploy.rb
@@ -1,3 +1,25 @@
 set :stages, %w(staging production)
 set :default_stage, "production"
+
+set :application, "PROJECT_NAME"
+
+# Git repository
+set :scm,                   :git
+set :repository,            "ssh://agilitic.com/git/#{application}.git"
+set :branch,                "master"
+set :deploy_via,            :remote_cache
+
+# Passenger reciepes
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
 require 'capistrano/ext/multistage'
--- files/config/deploy/production.rb
+++ files/config/deploy/production.rb
@@ -1,28 +1,9 @@
-set :application, "PROJECT_NAME"
-
-set :scm,                   :git
-set :repository,            "ssh://agilitic.com/git/#{application}.git"
-set :branch,                "master"
-set :deploy_via,            :remote_cache
-
 set :deploy_to, "/www/production/#{application}"
 
+# Target servers
 role :app, "agilitic.com"
 role :web, "agilitic.com"
 role :db,  "agilitic.com", :primary => true
 
 set :user,   "deploybot"
 set :runner, "deploybot"
-
-# Passenger reciepes
-namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-end
